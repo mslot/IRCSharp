df559d773083f06f41fba9aa0bbc2f2ef3bf8c98
Martin Slot <slot@sigsys.dk>
2012-07-10 22:30:15 +0200

    + Moved some classes out in a model project, and cleaned namespaces.

--- IRCSharp.Kernel.Model/Query/IRCCommandQuery.cs
+++ IRCSharp.Kernel.Model/Query/IRCCommandQuery.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace IRCSharp.Kernel.Query
+namespace IRCSharp.Kernel.Model.Query
 {
 	public class IRCCommandQuery
 	{
