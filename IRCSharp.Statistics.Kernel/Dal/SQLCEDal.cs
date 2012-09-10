e52cb2f9d4d92a58f7604463d09f51df871e26ee
Martin Slot <slot@sigsys.dk>
2012-09-10 19:30:32 +0200

    Raven is going to be seperated into a branch for itself. Removed SQLCE database. This is going to be setup using a migration tool.

---
+++ IRCSharp.Statistics.Kernel/Dal/SQLCEDal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRCSharp.Statistics.Kernel.Dal
+{
+	class SQLCEDal : IDal
+	{
+		private System.Data.SqlServerCe.SqlCeConnection _connection = null;
+		
+		public SQLCEDal(string connectionString)
+		{
+			_connection = new System.Data.SqlServerCe.SqlCeConnection(connectionString);
+			_connection.Open();
+		}
+
+		public void AddQuery(IRCSharp.Kernel.Model.Query.IRCCommandQuery query)
+		{
+			System.Data.SqlServerCe.SqlCeCommand command = _connection.CreateCommand();
+			if (DoesUserExists(query))
+			{
+				if (DoesChannelExists(query))
+				{
+					//create query
+				}
+				else
+				{
+					//create channel
+					//create query
+				}
+			}
+			else
+			{
+				//create user
+				//create channel
+				//create query
+			}
+
+		}
+
+		public Model.User GetUser(string nick)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			_connection.Dispose();
+		}
+	}
+}
